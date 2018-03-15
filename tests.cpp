3be5458011529084569aa2b8c2e0d7c745c791ed
Z.Shang <shangzhanlin@gmail.com>
2018-03-16 01:28:49 +0800

    add tests.cpp as an example

---
+++ tests.cpp
@@ -0,0 +1,38 @@
+#include "includes/core.hpp"
+#include "includes/hacks.hpp"
+#include "includes/list_utils.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace licpp;
+
+int main(int, char**){
+  var cons_1 = cons(1, 2);
+  var cons_2 = cons("foo", "bar");
+  var cons_3 = cons(1, "foo");
+  cout << "Cons 1: " << cons_1 << endl;
+  cout << "Cons 2: " << cons_2 << endl;
+  cout << "Cons 3: " << cons_3 << endl;
+
+  var list_1 = list(1, 2, 3);
+  var list_2 = cons("foo", list_1);
+  var list_3 = append(list_1, list_2);
+  var list_4 = reverse(list_1);
+  var list_5 = mapcar([](auto n) { return n + 1; }, list_1);
+  var list_6 = mapcar([](auto n, auto s) { return to_string(n) + s; }, list_1, list("a", "b", "c"));
+  cout << "List 1: " << list_1 << endl;
+  cout << "List 2: " << list_2 << endl;
+  cout << "List 3: " << list_3 << endl;
+  cout << "List 4: " << list_4 << endl;
+  cout << "List 5: " << list_5 << endl;
+  cout << "List 6: " << list_6 << endl;
+
+  int v1, v2, v3;
+  multiple_value_bind(list_5, &v1, &v2, &v3)([=]{
+      cout << "v1: " << v1 << " v2: " << v2 << " v3: " << v3 << endl;
+    });
+
+  return 0;
+}
