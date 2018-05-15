075739f3c07c5b77dcbe69c39ec2a9e6d51b010f
Z.Shang <shangzhanlin@gmail.com>
2018-05-15 20:02:31 +0800

    update license

--- includes/hacks.hpp
+++ includes/hacks.hpp
@@ -1,3 +1,9 @@
+/*
+ * This file is a part of the LICPP project,
+ * licensed under MIT liense.
+ * Please read LICENSE.TXT and README.MKD for more information.
+ */
+
 #ifndef __LICPP_HACKS__
 #define __LICPP_HACKS__
 
@@ -6,85 +12,85 @@
 
 namespace licpp{
   template <typename T>
-  struct _values_t {
-    using type = nullptr_t;
-  };
+    struct _values_t {
+      using type = nullptr_t;
+    };
   template <typename T>
-  struct _values_t <Cons<T, nil_t> * >{
-    using type = Cons<T * , nil_t> *;
-  };
+    struct _values_t <Cons<T, nil_t> * >{
+      using type = Cons<T * , nil_t> *;
+    };
   template <typename T, typename U>
-  struct _values_t <Cons<T, U> * > {
-    using type = Cons<T * , typename _values_t<U>::type> * ;
-  };
+    struct _values_t <Cons<T, U> * > {
+      using type = Cons<T * , typename _values_t<U>::type> * ;
+    };
 
   template <typename T, typename S = typename _values_t<Cons<T, nil_t> * >::type>
-  void _mvb(Cons<T, nil_t> * lst, S sym){
-    *(car(sym)) = car(lst);
-  }
+    void _mvb(Cons<T, nil_t> * lst, S sym){
+      *(car(sym)) = car(lst);
+    }
   template <typename T, typename U, typename S = typename _values_t<Cons<T, U> * >::type>
-  void _mvb(Cons<T, U> * lst, S sym){
-    *(car(sym)) = car(lst);
-    _mvb(cdr(lst), cdr(sym));
-  }
+    void _mvb(Cons<T, U> * lst, S sym){
+      *(car(sym)) = car(lst);
+      _mvb(cdr(lst), cdr(sym));
+    }
 
   template <typename L>
-  struct get_return : get_return<decltype(&L::operator())>
+    struct get_return : get_return<decltype(&L::operator())>
   {};
   template <typename C, typename ...A>
-  struct get_return <void (C::*)(A...)>{
-    template <typename L>
-    Cons<bool, nullptr_t> * operator()(L fn){
-      fn();
-      return cons(false, nullptr);
-    }
-  };
+    struct get_return <void (C::*)(A...)>{
+      template <typename L>
+        Cons<bool, nullptr_t> * operator()(L fn){
+          fn();
+          return cons(false, nullptr);
+        }
+    };
   template <typename R, typename C, typename ...A>
-  struct get_return <R (C::*)(A...)>{
-    template <typename L>
-    Cons<bool, R> * operator()(L fn){
-      return cons(true, fn());
-    }
-  };
+    struct get_return <R (C::*)(A...)>{
+      template <typename L>
+        Cons<bool, R> * operator()(L fn){
+          return cons(true, fn());
+        }
+    };
   template <typename C, typename ...A>
-  struct get_return <void (C::*)(A...) const>{
-    template <typename L>
-    Cons<bool, nullptr_t> * operator()(L fn){
-      fn();
-      return cons(false, nullptr);
-    }
-  };
+    struct get_return <void (C::*)(A...) const>{
+      template <typename L>
+        Cons<bool, nullptr_t> * operator()(L fn){
+          fn();
+          return cons(false, nullptr);
+        }
+    };
   template <typename R, typename C, typename ...A>
-  struct get_return <R (C::*)(A...) const>{
-    template <typename L>
-    Cons<bool, R> * operator()(L fn){
-      return cons(true, fn());
-    }
-  };
+    struct get_return <R (C::*)(A...) const>{
+      template <typename L>
+        Cons<bool, R> * operator()(L fn){
+          return cons(true, fn());
+        }
+    };
 
   template <typename T, typename U, typename ... Ss>
-  auto multiple_value_bind(Cons<T, U> * c, Ss ... sym){
-    if(!std::is_same<typename _values_t<Cons<T, U> * >::type,
-       typename _list_t<Ss ...>::type>::value){
-      throw "The type of Pointer-List does not match the type of the given List";
-    }
-    if(!listp(c)){
-      throw "MULTIPLE-VALUE-BIND requires a proper list as its first argument.";
-    }
-    using sym_lst_t = typename _values_t<Cons<T, U> * >::type;
-    sym_lst_t sym_lst = list(sym...);
-    auto orig = mapcar([](auto p){ return *p; }, sym_lst);
-    _mvb(c, sym_lst);
-    return [orig, sym_lst](auto fn){
-      auto retval = get_return<decltype(fn)>()(fn);
-      _mvb(orig, sym_lst);
-      if(car(retval)){
-        return cdr(retval);
+    auto multiple_value_bind(Cons<T, U> * c, Ss ... sym){
+      if(!std::is_same<typename _values_t<Cons<T, U> * >::type,
+          typename _list_t<Ss ...>::type>::value){
+        throw "The type of Pointer-List does not match the type of the given List";
       }
-      typename _cdr_t<decltype(retval)>::type ret = 0;
-      return ret;
-    };
-  }
+      if(!listp(c)){
+        throw "MULTIPLE-VALUE-BIND requires a proper list as its first argument.";
+      }
+      using sym_lst_t = typename _values_t<Cons<T, U> * >::type;
+      sym_lst_t sym_lst = list(sym...);
+      auto orig = mapcar([](auto p){ return *p; }, sym_lst);
+      _mvb(c, sym_lst);
+      return [orig, sym_lst](auto fn){
+        auto retval = get_return<decltype(fn)>()(fn);
+        _mvb(orig, sym_lst);
+        if(car(retval)){
+          return cdr(retval);
+        }
+        typename _cdr_t<decltype(retval)>::type ret = 0;
+        return ret;
+      };
+    }
 };
 
 #endif
--- includes/list_utils.hpp
+++ includes/list_utils.hpp
@@ -1,3 +1,9 @@
+/*
+ * This file is a part of the LICPP project,
+ * licensed under MIT liense.
+ * Please read LICENSE.TXT and README.MKD for more information.
+ */
+
 #ifndef __LICPP_LIST_UTILS__
 #define __LICPP_LIST_UTILS__
 
@@ -7,145 +13,145 @@
 
 namespace licpp{
   template <typename T, typename U>
-  struct _append_t{
-    using type = nullptr_t;
-  };
+    struct _append_t{
+      using type = nullptr_t;
+    };
   template <typename T, typename U>
-  struct _append_t<nil_t, Cons<T, U> * >{
-    using type = Cons<T, U> *;
-  };
+    struct _append_t<nil_t, Cons<T, U> * >{
+      using type = Cons<T, U> *;
+    };
   template <typename T, typename U>
-  struct _append_t<Cons<T, U> *, nil_t>{
-    using type = Cons<T, U> *;
-  };
+    struct _append_t<Cons<T, U> *, nil_t>{
+      using type = Cons<T, U> *;
+    };
   template <typename T, typename S, typename Y>
-  struct _append_t<T, Cons<S, Y> * >{
-    using type = Cons<T, Cons<S, Y> * > * ;
-  };
+    struct _append_t<T, Cons<S, Y> * >{
+      using type = Cons<T, Cons<S, Y> * > * ;
+    };
   template <typename T, typename S, typename Y>
-  struct _append_t<Cons<T, nil_t> * , Cons<S, Y> * >{
-    using type = Cons<T, Cons<S, Y> * > * ;
-  };
+    struct _append_t<Cons<T, nil_t> * , Cons<S, Y> * >{
+      using type = Cons<T, Cons<S, Y> * > * ;
+    };
   template <typename T, typename U, typename S, typename Y>
-  struct _append_t<Cons<T, U> *, Cons<S, Y> * >{
-    using type = Cons<T, typename _append_t<U, Cons<S, Y> * >::type> *;
-  };
+    struct _append_t<Cons<T, U> *, Cons<S, Y> * >{
+      using type = Cons<T, typename _append_t<U, Cons<S, Y> * >::type> *;
+    };
   template <typename S, typename Y>
-  Cons<S, Y> * _append(nil_t, Cons<S, Y> * b){
-    return b;
-  }
-  template <typename T, typename U>
-  Cons<T, U> * _append(Cons<T, U> * a, nil_t){
-    return a;
-  }
-  template <typename T, typename U, typename S, typename Y,
-            typename R = typename _append_t<Cons<T, U> *, Cons<S, Y> *>::type>
-  R _append(Cons<T, U> * a, Cons<S, Y> * b){
-    return cons(car(a), _append(cdr(a), b));
-  }
-  template <typename T, typename U, typename S, typename Y>
-  auto append(Cons<T, U> * a, Cons<S, Y> * b){
-    if(listp(a) && listp(b)){
-      return _append(a, b);
+    Cons<S, Y> * _append(nil_t, Cons<S, Y> * b){
+      return b;
     }
-    throw "APPEND only works on proper lists.";
-  }
+  template <typename T, typename U>
+    Cons<T, U> * _append(Cons<T, U> * a, nil_t){
+      return a;
+    }
+  template <typename T, typename U, typename S, typename Y,
+           typename R = typename _append_t<Cons<T, U> *, Cons<S, Y> *>::type>
+             R _append(Cons<T, U> * a, Cons<S, Y> * b){
+               return cons(car(a), _append(cdr(a), b));
+             }
+  template <typename T, typename U, typename S, typename Y>
+    auto append(Cons<T, U> * a, Cons<S, Y> * b){
+      if(listp(a) && listp(b)){
+        return _append(a, b);
+      }
+      throw "APPEND only works on proper lists.";
+    }
 
   template <typename T>
-  struct _reverse_t {
-    using type = nullptr_t;
-  };
+    struct _reverse_t {
+      using type = nullptr_t;
+    };
   template <>
-  struct _reverse_t <nil_t> {
-    using type = nil_t;
-  };
+    struct _reverse_t <nil_t> {
+      using type = nil_t;
+    };
   template <typename T>
-  struct _reverse_t <Cons<T, nil_t> *>{
-    using type = Cons<T, nil_t> *;
-  };
+    struct _reverse_t <Cons<T, nil_t> *>{
+      using type = Cons<T, nil_t> *;
+    };
   template <typename T, typename U>
-  struct _reverse_t <Cons<T, Cons<U, nil_t> * > *>{
-    using type = Cons<U, Cons<T, nil_t> *> *;
-  };
+    struct _reverse_t <Cons<T, Cons<U, nil_t> * > *>{
+      using type = Cons<U, Cons<T, nil_t> *> *;
+    };
   template <typename T, typename U>
-  struct _reverse_t <Cons<T, U> *>{
-    using type = typename _append_t<typename _reverse_t<U>::type, Cons<T, nil_t> * >::type;
-  };
+    struct _reverse_t <Cons<T, U> *>{
+      using type = typename _append_t<typename _reverse_t<U>::type, Cons<T, nil_t> * >::type;
+    };
   nil_t _reverse(nil_t){
     return nil;
   }
   template <typename T, typename U, typename R = typename _reverse_t<Cons<T, U> *>::type>
-  R _reverse(Cons<T, U> * lst){
-    return append(_reverse(cdr(lst)), list(car(lst)));
-  }
-  template <typename T, typename U>
-  auto reverse(Cons<T, U> * lst){
-    if(listp(lst)){
-      return _reverse(lst);
+    R _reverse(Cons<T, U> * lst){
+      return append(_reverse(cdr(lst)), list(car(lst)));
     }
-    throw "APPEND only works on proper lists.";
-  }
+  template <typename T, typename U>
+    auto reverse(Cons<T, U> * lst){
+      if(listp(lst)){
+        return _reverse(lst);
+      }
+      throw "APPEND only works on proper lists.";
+    }
 
   template <typename T, typename U, typename ...Us>
-  struct _mapcar_t {};
+    struct _mapcar_t {};
   template <typename F>
-  struct _mapcar_t<F, nil_t>{
-    using type = nil_t;
-  };
+    struct _mapcar_t<F, nil_t>{
+      using type = nil_t;
+    };
   template <typename F, typename T>
-  struct _mapcar_t<F, Cons<T, nil_t> *>{
-    using type = Cons<typename std::result_of<F&(T)>::type, nil_t> *;
-  };
+    struct _mapcar_t<F, Cons<T, nil_t> *>{
+      using type = Cons<typename std::result_of<F&(T)>::type, nil_t> *;
+    };
   template <typename F, typename T, typename U>
-  struct _mapcar_t<F, Cons<T, U> *>{
-    using type = Cons<typename std::result_of<F&(T)>::type, typename _mapcar_t<F, U>::type> * ;
-  };
+    struct _mapcar_t<F, Cons<T, U> *>{
+      using type = Cons<typename std::result_of<F&(T)>::type, typename _mapcar_t<F, U>::type> * ;
+    };
   template <typename F, typename ...S>
-  struct _packed_result_t{
-    using type = typename std::result_of<F&(S...)>::type;
-  };
+    struct _packed_result_t{
+      using type = typename std::result_of<F&(S...)>::type;
+    };
   template <typename F, typename T, typename ...S>
-  struct _mapcar_t<F, Cons<T, nil_t> *, S...>{
-    using type = Cons<typename _packed_result_t<F, T, typename _car_t<S>::type ...>::type,
-                      nil_t> * ;
-  };
+    struct _mapcar_t<F, Cons<T, nil_t> *, S...>{
+      using type = Cons<typename _packed_result_t<F, T, typename _car_t<S>::type ...>::type,
+            nil_t> * ;
+    };
   template <typename F, typename T, typename U, typename ...S>
-  struct _mapcar_t<F, Cons<T, U> *, S...>{
-    using type = Cons<typename _packed_result_t<F, T, typename _car_t<S>::type ...>::type,
-                      typename _mapcar_t<F, U, typename _cdr_t<S>::type ...>::type> *;
-  };
+    struct _mapcar_t<F, Cons<T, U> *, S...>{
+      using type = Cons<typename _packed_result_t<F, T, typename _car_t<S>::type ...>::type,
+            typename _mapcar_t<F, U, typename _cdr_t<S>::type ...>::type> *;
+    };
 
   template <typename F, typename T>
-  auto _mapcar(F fn, Cons<T, nil_t> * lst){
-    return list(fn(car(lst)));
-  }
+    auto _mapcar(F fn, Cons<T, nil_t> * lst){
+      return list(fn(car(lst)));
+    }
   template <typename F, typename T, typename U,
-            typename R = typename _mapcar_t<Cons<T, U>*, F>::type>
-  R _mapcar(F fn, Cons<T, U> * lst){
-    return cons(fn(car(lst)), _mapcar(fn, cdr(lst)));
-  }
+           typename R = typename _mapcar_t<Cons<T, U>*, F>::type>
+             R _mapcar(F fn, Cons<T, U> * lst){
+               return cons(fn(car(lst)), _mapcar(fn, cdr(lst)));
+             }
   template <typename F, typename T, typename ...S>
-  auto _mapcar(F fn, Cons<T, nil_t> * lst, S... rest){
-    return list(fn(car(lst), car(rest)...));
-  }
+    auto _mapcar(F fn, Cons<T, nil_t> * lst, S... rest){
+      return list(fn(car(lst), car(rest)...));
+    }
   template <typename F, typename T, typename U, typename ...S>
-  auto _mapcar(F fn, Cons<T, U> * lst, S... rest){
-    return cons(fn(car(lst), car(rest)...), _mapcar(fn, cdr(lst), cdr(rest)...));
-  }
+    auto _mapcar(F fn, Cons<T, U> * lst, S... rest){
+      return cons(fn(car(lst), car(rest)...), _mapcar(fn, cdr(lst), cdr(rest)...));
+    }
   template <typename F, typename T, typename U>
-  auto mapcar(F fn, Cons<T, U> * lst){
-    if(listp(lst)){
-      return _mapcar(fn, lst);
+    auto mapcar(F fn, Cons<T, U> * lst){
+      if(listp(lst)){
+        return _mapcar(fn, lst);
+      }
+      throw "MAPCAR only works on proper list.";
     }
-    throw "MAPCAR only works on proper list.";
-  }
   template <typename F, typename T, typename U, typename ...S>
-  auto mapcar(F fn, Cons<T, U> * lst, S... rest){
-    if(listp(lst)){
-      return _mapcar(fn, lst, rest...);
+    auto mapcar(F fn, Cons<T, U> * lst, S... rest){
+      if(listp(lst)){
+        return _mapcar(fn, lst, rest...);
+      }
+      throw "MAPCAR only works on proper list.";
     }
-    throw "MAPCAR only works on proper list.";
-  }
 };
 
 #endif
